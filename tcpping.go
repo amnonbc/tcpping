230b7d917bc41c221a7e544fc048070d53d85b8c
amnon <amnonbc@gmail.com>
2024-03-24 08:50:32 +0000

    enc ts and len

--- tcpping.go
+++ tcpping.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"io"
@@ -19,6 +21,7 @@ var (
 )
 
 func main() {
+	log.SetFlags(log.Lmicroseconds)
 	flag.Parse()
 	if *server {
 		doServer()
@@ -38,17 +41,29 @@ func doClient() {
 	defer conn.Close()
 
 	tick := time.NewTicker(*delay)
-	buf := make([]byte, *sz)
+	sbuf := make([]byte, *sz)
+	rbuf := make([]byte, *sz)
 	for range tick.C {
-		start := time.Now()
-		n, err := conn.Write(buf)
+		binary.LittleEndian.PutUint64(sbuf, uint64(*sz))
+		t, _ := time.Now().MarshalBinary()
+		copy(sbuf[8:], t)
+		n, err := conn.Write(sbuf)
 		if err != nil {
 			log.Fatal(err)
 		}
 		log.Println("wrote", n, "bytes")
-		n, err = io.ReadFull(conn, buf)
+		n, err = io.ReadFull(conn, rbuf)
 		if err != nil {
 			log.Fatal(err)
+		}
+		var start time.Time
+		const timeStampLen = 15
+		if len(rbuf) < 8+timeStampLen {
+			log.Fatalln("rbuf too short")
+		}
+		err = start.UnmarshalBinary(rbuf[8 : 8+timeStampLen])
+		if err != nil {
+			log.Fatalln("could not parse time", err)
 		}
 		log.Println("read", n, "bytes, delay", time.Since(start))
 	}
@@ -60,6 +75,7 @@ func doServer() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("listening on", l.Addr())
 	defer l.Close()
 	for {
 		conn, err := l.Accept()
@@ -70,14 +86,44 @@ func doServer() {
 	}
 }
 
+type lreader struct {
+	r io.Reader
+}
+
+func (l *lreader) Read(buf []byte) (int, error) {
+	n, err := l.r.Read(buf)
+	log.Println("read", n, "bytes from socket")
+	if err != nil {
+		log.Println(err)
+	}
+	return n, err
+}
+
 func handleConnection(c net.Conn) {
+	log.Println("got connection from", c.RemoteAddr())
+	bc := bufio.NewReaderSize(&lreader{c}, 64*1028)
+	szBuf := make([]byte, 8)
+
 	buf := make([]byte, *sz)
 	for {
-		n, err := io.ReadFull(c, buf)
+		_, err := io.ReadFull(bc, szBuf)
+		if err != nil {
+			log.Println("reading size", err)
+		}
+		if err == io.EOF {
+			return
+		}
+		sz := binary.LittleEndian.Uint64(szBuf)
+		log.Println("reading", sz)
+		if len(buf) != int(sz) {
+			buf = make([]byte, int(sz))
+		}
+		n, err := io.ReadFull(bc, buf[8:])
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println("got", n, "bytes")
+		log.Println("got", n+8, "bytes")
+		copy(buf, szBuf)
 		c.Write(buf)
 	}
 }
